c337437a5fef7794eff7d73df93eaedfbfbb6974
mjsadeghinia <mjsadeghinia@outlook.com>
2024-10-23 12:28:57 +0000

    update the pipeline to have json files as settings for each case

--- create_mesh.py
+++ create_mesh.py
@@ -1,39 +1,17 @@
 from pathlib import Path
 import argparse
+import json
 from structlog import get_logger
 
 import meshing_utils
 
 logger = get_logger()
 
-# %%
-data_address = Path("/home/shared/00_data/MAD_4/")
-
-
-def get_default_mesh_settings():
-    return {
-        "seed_num_base_epi": 100,
-        "seed_num_base_endo": 100,
-        "num_z_sections_epi": 50,
-        "num_z_sections_endo": 50,
-        "num_mid_layers_base": 7,
-        "smooth_level_epi": 0.1,
-        "smooth_level_endo": 0.1,
-        "num_lax_points": 64,
-        "lax_smooth_level_epi": 20,
-        "lax_smooth_level_endo": 20,
-        "z_sections_flag_epi": 0,
-        "z_sections_flag_endo": 0,
-        "seed_num_threshold_epi": 25,
-        "seed_num_threshold_endo": 20,
-        "scale_for_delauny": 1.5,
-        "t_mesh": -1,
-        "MeshSizeMin": 0.5,
-        "MeshSizeMax": 1,
-        "SurfaceMeshSizeEndo": 2,
-        "SurfaceMeshSizeEpi": 2,
-    }
-
+def load_settings(setting_dir, sample_name):
+    settings_fname = setting_dir / f"{sample_name}.json"
+    with open(settings_fname, "r") as file:
+        settings = json.load(file)
+    return settings
 
 def main(args=None) -> int:
     """
@@ -57,6 +35,21 @@ def main(args=None) -> int:
         type=Path,
         help="The directory where all the patients data stored.",
     )
+    
+    parser.add_argument(
+        "--settings_dir",
+        default="/home/shared/MAD-SSA/settings",
+        type=Path,
+        help="The settings directory where json files are stored.",
+    )
+    
+    parser.add_argument(
+        "-m",
+        "--mesh_quality",
+        default='fine',
+        type=str,
+        help="The mesh quality. Settings will be loaded accordingly from json file",
+    )
 
     parser.add_argument(
         "-o",
@@ -70,11 +63,14 @@ def main(args=None) -> int:
     sample_name = args.name
     data_directory = args.data_directory
     output_folder = args.output_folder
-
-    mesh_settings = get_default_mesh_settings()
+    settings_dir = args.settings_dir
+    mesh_quality = args.mesh_quality
+    
+    settings = load_settings(settings_dir, sample_name)
+    mesh_settings = settings["mesh"][mesh_quality]
 
     sample_directory = data_directory / sample_name
-    LVmesh = meshing_utils.create_mesh(mesh_settings, sample_directory, output_folder)
+    output_mesh_filename = meshing_utils.create_mesh(mesh_settings, sample_directory, output_folder)
 
 
 if __name__ == "__main__":
