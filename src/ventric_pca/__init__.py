95f5b1ddc0ee0a8857aeca1cdd1b443f4109c877
mjsadeghinia <mjsadeghinia@outlook.com>
2025-06-16 08:28:55 +0000

    added init file for packaging

---
+++ src/ventric_pca/__init__.py
@@ -0,0 +1,17 @@
+"""
+ventric_pca
+===========
+
+A small package for ventricular pointcloud analysis.
+"""
+
+__version__ = "0.1.0"
+
+from . import get_rv
+from . import alignment
+from . import meshing_utils
+from . import create_3d_mesh
+from . import create_point_cloud
+from . import main
+from . import utils
+from . import pca
